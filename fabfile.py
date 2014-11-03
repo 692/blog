f82def6ba68fce50a64271c8ee49bced83629928
692 <6889692@gmail.com>
2014-11-03 14:28:28 +0800

    123

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ import fabric.contrib.project as project
 env.hosts = ["192.81.133.96"]
 env.user = "root"
 env.colorize_errors = True
-env.local_output = "_site/?/blog/"
+env.local_output = "_site/"
 env.remote_output = "/opt/blog.tankywoo.com/"
 env.rsync_delete = False
 
