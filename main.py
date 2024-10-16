268d5fd8b43f16f7e82c932b84ceaf3b7a1af86e
692 <6889692@gmail.com>
2024-10-16 10:22:04 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -301,7 +301,7 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
 
 def save_issue(issue, me, dir_name=BACKUP_DIR):
     md_name = os.path.join(
-        dir_name, f"0000-00-00-{issue.title.replace('/', '-').replace(' ', '.')}.md"
+        dir_name, f"2024-01-01-{issue.title.replace('/', '-').replace(' ', '.')}.md"
     )
     with open(md_name, "w") as f:
         #f.write(f"# [{issue.title}]({issue.html_url})\n\n")
